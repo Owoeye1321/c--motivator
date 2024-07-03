25661363c4b190311ee9e2ec3cf9967b6d13d8b4
Owoeye Samuel <owoeye1321@gmail.com>
2024-07-03 21:36:19 +0100

    holy shit! thank goodness on first depression lifting

---
+++ Interface/MotivatorInterface.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LiftDepression.Interface
+{
+	public interface IMotivatorInterface
+	{
+		string title { get; set; }
+		string note { get; set; }
+		string author { get; set; }
+	}
+}
+
---
+++ Service/MotivatorService.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace LiftDepression.Service
+{
+	public class MotivatorService
+	{
+		public MotivatorService()
+		{
+		}
+	}
+}
+
